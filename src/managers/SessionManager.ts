18ea3823faadba18f4602b99ebc9316397c21e72
Camilo <camilolillo@gmail.com>
2024-10-03 18:16:55 -0300

    Firebase Auth service done. Pending, implement it.

--- src/managers/SessionManager.ts
+++ src/managers/SessionManager.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
-import firebase from 'firebase/compat';
 
+//imports firebase
+import firebase from 'firebase/compat';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class SessionManager {
+
+    //agrego constructor
+    constructor(public fireAuth: AngularFireAuth) { }
+
 
     private readonly temporaryUserName: string = 'user';
     private readonly temporaryPass: string = 'pass';
@@ -19,7 +25,23 @@ export class SessionManager {
         }  
     }
 
-    performLogout() {
-        //TODO
+    async signOut() {
+        return await this.fireAuth.signOut()
+    }
+
+    async registerUserWith(email: string, password: string) {
+        return await this.fireAuth.createUserWithEmailAndPassword(email, password)
+    }
+
+    async loginWith(email: string, password: string) {
+        return await this.fireAuth.signInWithEmailAndPassword(email, password)
+    }
+
+    async resetPassword(email: string) {
+        return await this.fireAuth.sendPasswordResetEmail(email)
+    }
+
+    async getProfile() {
+        return await this.fireAuth.currentUser
     }
 }
