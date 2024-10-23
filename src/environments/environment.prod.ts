07b47c27ee82ec9382b0f0de8b37ce549b6dc494
Camilo <camilolillo@gmail.com>
2024-10-23 14:19:16 -0300

    Environments updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyAs23Te1KQpkadfclC7nKlUoebPqpTgbDg",
+      authDomain: "ionictestapp-6355f.firebaseapp.com",
+      projectId: "ionictestapp-6355f",
+      storageBucket: "ionictestapp-6355f.appspot.com",
+      messagingSenderId: "917555013625",
+      appId: "1:917555013625:web:2f6ceeb25d578f020bbf28"
+    },
+    apiBaseUrl: 'https://rickandmortyapi.com/api/',
+    apiEndpoints: {
+      character: 'character/',
+      location: 'location/',
+      episode: 'episode/'
+    }
 }
